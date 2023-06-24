f7320304a4eb3832cb514de4f3850ff73f84ccd2
Juan Sebastián Rubio <sebarubio@gmail.com>
2023-06-24 00:16:10 -0300

    Codigo JS para productos.html con Prompts para compras

--- js/script.js
+++ js/script.js
@@ -1,31 +1,78 @@
-alert("Bienvenido. Por favor ingresa el café que quieras y la cantidad para que lo sumemos a tu carrito. Una vez terminado, ingresa la palabra SALIDA para darte el total. Gracias por elegirnos!")
-let articulo = "";
-let lista_articulos = "";
-let total_compra = 0;
-while (articulo!="SALIDA"){ 
-    articulo = prompt("Ingrese el codigo del articulo");
-    switch (articulo){
-      case "1":
-        lista_articulos += "Aceite - $1500" + "<br>";
-        total_compra += 1500;
-        break;
-      case "2":
-        lista_articulos += "Papa - $700" + "<br>";
-        total_compra += 700;
-        break;
-      case "3":
-        lista_articulos += "Cebolla - $500" + "<br>";
-        total_compra += 500;
-        break;
-      case "4":
-        lista_articulos += "Fernet - $5800" + "<br>";
-        total_compra += 800;
-        break;
+alert("Bienvenido a nuestra página de Productos. Podrás realizar tus compras aquí.");
+alert("Una vez que hayas terminado de elegir tus productos, ingresa la palabra SALIDA para darte el total de tu compra. Gracias por elegirnos!");
+
+function confirmacion(next){
+    if(next == "SI"){
+      return true;
+    }else{
+      return false;
+    }
+}
+let next = "";
+next = prompt("¿Quieres realizar una compra? SI/NO");
+let seguir = confirmacion(next);
+
+if (seguir){
+  let producto = "";
+  let lista_productos = "";
+  let total_compra = 0;
+  while (producto!="SALIDA"){ 
+    producto = prompt("Ingrese el codigo del producto. Ej: 1. SALIDA para finalizar la compra.");
+    switch (producto){
       default:
         alert ("El código ingresado no existe");
+      case "1":
+        total_compra += 18;
+        break;
+      case "2":
+        total_compra += 21;
+        break;
+      case "3":
+        total_compra += 23;
+        break;
+      case "4":
+        total_compra += 25;
+        break;
+      case "5":
+        total_compra += 27;
+        break;
+      case "6":
+        total_compra += 27;
+        break;
+      case "7":
+        total_compra += 29;
+        break;
+      case "8":
+        total_compra += 29;
+        break;
+      case "9":
+        total_compra += 29;
+        break;
+      case "SALIDA":
+        alert ("COMPRA FINALIZADA. AGUARDE UN MOMENTO.");
     }
   }
-  alert("El total de la compra es:"+total_compra);
+  if (total_compra == 0){
+    alert('No hay artículos en su carrito');
+  }else{
+    total = parseFloat(total_compra)
+    alert("El total de la compra es: €"+total);
+  }
+}else{
+  alert("Si quieres realizar una compra, actualiza la página. Gracias por visitarnos!")
+}
+
+/*        if(seguir2){
+          alert ("COMPRA FINALIZADA. AGUARDE UN MOMENTO.");
+        }else{
+          break;
+        }*/
+
+
+
+
+
+
 // Hacer Do while
 
 
